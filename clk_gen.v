857244fca51fb64ea1975efac0ecb0ff0c705dac
Paszti90 <paszti90@gmail.com>
2017-05-08 22:15:26 +0200

    Add files via upload

--- clk_gen.v
+++ clk_gen.v
@@ -15,38 +15,38 @@
 --  ====
 --
 -----------------------------------------------------------------------------------------------------------------------*/
-`include "utils.v"
-
 module CLK_gen (
 	input 			clk_i,
 	input				reset_n_i,
-	input [7:0]		clk_div_i,
+	input				clk_en,
+	input [3:0]		clk_div_i,
 	output 			clk_o
 );
+	    
+	reg clk_y;
+	reg [3:0] clk_cnt_y;
 	
-	reg [7:0] clk_cnt_y;
-	wire [7:0] clk_cnt_next_y;
-	reg clk_en_y;
-	
-	always @(posedge clk_i or negedge reset_n_i)
+	always@(posedge clk_i,negedge reset_n_i)
 	begin
 		if (!reset_n_i)
-			clk_cnt_y <= 0;
-		else
 		begin
-			if (clk_cnt_y == clk_div_i)
-			begin
-				clk_cnt_y <= 0;
-				clk_en_y <= ~clk_en_y;
-			end
-			else
-				clk_cnt_y <= clk_cnt_next_y;
+			clk_cnt_y <= 0;
+			clk_y <= 0;
 		end
+		else if(clk_en)
+		begin
+			if(clk_cnt_y == clk_div_i-1)
+			begin
+				clk_y <= ~clk_y; 
+				clk_cnt_y <= 0;
+			end
+			else 
+				clk_cnt_y <= clk_cnt_y +1;
+		end
+		else
+			clk_y <= 0;
 	end
 	
-	`TRUNC(trunc_signal, 8, 7);
-	assign clk_cnt_next_y = trunc_signal(clk_cnt_y + 1);
-	 	      
-	assign clk_o = clk_en_y;
+	assign clk_o = (clk_div_i==0) ? clk_i : clk_y;
 
 endmodule
--- spi_master.v
+++ spi_master.v
@@ -39,7 +39,6 @@
 -- * Generate trans_done_o
 -- * Control FIFO outputs
 -- * Testing, commenting, clean up
--- * QSPI / SPI mode
 -----------------------------------------------------------------------------------------------------------------------*/
 
 module spi_master (
@@ -77,17 +76,15 @@ module spi_master (
 	/*=============================================================================================
     --  CLOCK GENERATION
     --=============================================================================================*/
-	wire spi_sclk_y;
 	wire sclk_y;
-	wire [7:0] clk_div_y;
+	wire [3:0] clk_div_y;
 	wire cpol_y;
+	reg sclk_en_reg_y;
 	
-	assign clk_div_y = reg_control_i[7:0];
+	assign clk_div_y = reg_control_i[3:0];
 	assign cpol_y = reg_control_i[9];
-	
-	CLK_gen sclk_gen(clk_i, reset_n_i, clk_div_y, sclk_y);
-	
-	assign spi_sclk_y = (!cpol_y) ? sclk_y : ~sclk_y;
+		
+	CLK_gen sclk_gen(clk_i, sclk_en_reg_y,reset_n_i, clk_div_y, sclk_y);
 	
 	reg prev_sclk_y;
 	always @(posedge clk_i) prev_sclk_y <= sclk_y;
@@ -101,86 +98,291 @@ module spi_master (
 			sclk_rise_y <= 0;
 	end;
 	
-
-    /*=============================================================================================
-    -- Sampling clock enable generation: generate 'samp_ce_y' from 'clk_en_y' depending on CPHA
-    -- always sample data at the half-cycle of the fsm update cell
-	 --=============================================================================================*/
+	reg sclk_fall_y;
+	always @(posedge clk_i) 
+	begin
+		if (prev_sclk_y & !sclk_y) // falling edge of SCLK
+			sclk_fall_y <= 1;
+		else
+			sclk_fall_y <= 0;
+	end;
+//    /*=============================================================================================
+//    -- Sampling clock enable generation: depending on CPHA
+//	 --=============================================================================================*/
 	wire samp_ce_y;
-	wire fsm_ce_y;
 	wire cpha_y;
 	
 	assign cpha_y = reg_control_i[10];
-	assign samp_ce_y = (!cpha_y) ? sclk_rise_y : ~sclk_rise_y;
-	assign fsm_ce_y = (!cpha_y) ? ~sclk_rise_y : sclk_rise_y;
+	
+	assign samp_ce_y = ((sclk_en_reg_y) & (state == TRANSFER)) ? ((!cpha_y) ? sclk_rise_y : sclk_fall_y) : 0;
 	
 	/*=============================================================================================
    -- REGISTERED INPUTS
-   --=============================================================================================
-    // rx bit flop: capture rx bit after SAMPLE edge of sck */
-	reg rx_bit_reg_y;
+   --=============================================================================================*/
+   
+	 reg rx_bit_reg_y;
 	
-   always @(posedge clk_i)
-	begin
-        if (samp_ce_y)
-		  begin
-            rx_bit_reg_y <= spi_miso_i;
-			end
-	end
+    always @(posedge clk_i)
+	 begin
+		rx_bit_reg_y <= spi_miso_i;
+	 end
 
     /*=============================================================================================
     --  REGISTER TRANSFER FSM
     --=============================================================================================*/
     // fsm state and data registers: synchronous to the spi base reference clock
 	
-	parameter [2:0]
+	reg [31:0] tx_reg_y;
+	reg [31:0] rx_reg_y;
+	reg tx_pull_y; 
+	reg rx_push_y;
+//	reg spi_enable_y;
+	reg transfer_en_y;
+	reg transfer_done_y;
+	reg [5:0] num_of_transferred_bits_y;
+	
+	wire msb_first;
+	assign msb_first = reg_control_i[8];
+	reg [5:0] bits;
+	
+
+	always@(*)
+	begin
+		case(reg_trans_ctrl_i[6:5]) 
+			2'b00 : bits <= 8;
+			2'b01 : bits <= 16;
+			2'b10 : bits <= 32;
+			default : bits <= 8;
+		endcase
+	end
+		
+	localparam [2:0]
 		IDLE = 3'd0,
-		WRITE = 3'd1,
-		S2 = 3'd2,
-		S3 = 3'd3,
-		ERROR = 3'd4;
+		LOAD = 3'd1, 
+		TRANSFER = 3'd2;
+	//	ERROR = 3'd3;
 		
 
 	reg [2:0]
-		state, next;
-	
+		state, next_state;
 	// FF registers clocked on rising edge and cleared on sync rst_i
-    always @(posedge clk_i or negedge reset_n_i)
+   always @(posedge clk_i, negedge reset_n_i)
 	begin
 		if (!reset_n_i)
 			state <= IDLE;
-      else 
-		begin
-			if (fsm_ce_y)
-				state <= next;
-		end
+      else
+			state <= next_state;
 	end
 
-	reg [31:0] tx_reg_y;
-	reg [31:0] rx_reg_y;
-	reg tx_pull_y;
-	reg sck_enable_y; 
-
-	always @(posedge clk_i)
+	always @(posedge clk_i, negedge reset_n_i)
     begin
-		
-		case (state)
-			IDLE : 
-				begin
-					tx_pull_y <= 0;
-					sck_enable_y <= 0;
-					if (trans_start_i & !tx_empty_i)
-						next <= WRITE;
-				end
-		endcase
+		if(!reset_n_i)
+			begin
+				tx_pull_y <= 0;
+				transfer_en_y <= 0;
+				num_of_transferred_bits_y <= 0;
+				transfer_done_y <= 0;
+				sclk_en_reg_y <= 0;
+				rx_push_y <= 0;
+				sclk_en_reg_y <= 0;
+				next_state <= IDLE;
+			end
+		else
+			case (state)
+				IDLE : 
+					begin
+						tx_pull_y <= 0;
+						transfer_en_y <= 0;
+						transfer_done_y <= 0;
+						sclk_en_reg_y <= 0;
+						rx_push_y <= 0;
+						if (trans_start_i & !tx_empty_i)
+						begin
+							tx_pull_y <= 1;
+							num_of_transferred_bits_y <= bits;
+							next_state <= LOAD;
+						end
+					end
+				LOAD : 
+					begin
+						tx_pull_y <= 0;
+						rx_push_y <= 0;
+						transfer_en_y <= 1;
+						next_state <= TRANSFER;
+					end
+				TRANSFER :
+					begin
+						rx_push_y <= 0;
+						tx_pull_y <= 0;
+						sclk_en_reg_y <= 1;
+						if(bit_counter==num_of_transferred_bits_y)
+						begin
+							sclk_en_reg_y <= 0;
+							if(tx_empty_i)
+							begin
+								transfer_done_y <= 1;
+								next_state <= IDLE;
+							end
+							else			
+							begin
+								tx_pull_y <= 1;
+								next_state <= LOAD;	
+								sclk_en_reg_y <= 0;
+							end
+							rx_push_y <= 1;
+						end
+					end
+					
+			endcase
 	end	
 	
-    /*=============================================================================================
-    -- SCK out logic: pipeline phase compensation for the SCK line
-    =============================================================================================*/	
-
+//    /*=============================================================================================
+//    -- Data shifting
+//    =============================================================================================*/		
 	
-	assign spi_sck_o = (sck_enable_y) ? spi_sclk_y : cpol_y;
+	reg [31:0] shr;
+	reg [5:0] bit_counter;
+	
+//	always @(posedge clk_i)
+//   begin
+//		if(!reset_n_i)
+//		begin
+//			shr <= 0;
+//			bit_counter <= 0;
+//			mosi_y <= 0;
+//		end
+//		else if(tx_pull_y)
+//			begin
+//				shr <= tx_data_i;
+//				if(msb_first)
+//					mosi_y <= tx_data_i[31];
+//				else
+//					mosi_y <= tx_data_i[0];
+//				bit_counter <=0;
+//			end
+//		else if(transfer_en_y)
+//			begin
+//				if(samp_ce_y)
+//				begin
+//					if(msb_first)
+//					begin
+//						shr <= {shr[30:0],spi_miso_i};
+//						mosi_y <= shr[31];
+//					end
+//					else 
+//					begin
+//						shr <= {spi_miso_i,shr[31:1]};
+//						mosi_y <= shr[0];
+//					end
+//					bit_counter <= bit_counter+1;
+//				end
+//			end
+//	end
+	
+	reg [7:0] fifo0,fifo1,fifo2,fifo3;
+	//reg [7:0] tx_fifo0,tx_fifo1,tx_fifo2,tx_fifo3;
+	//reg [7:0] rx_fifo0,rx_fifo1,rx_fifo2,rx_fifo3;
+	
+	always@(posedge clk_i,negedge reset_n_i)
+	begin
+		if(!reset_n_i)
+		begin
+			fifo0 <= 0;
+			fifo1 <= 0;
+			fifo2 <= 0;
+			fifo3 <= 0;	
+			bit_counter <= 0;
+		end
+		else if(tx_pull_y)
+		begin
+			fifo0 <= tx_data_i[7:0];
+			fifo1 <= tx_data_i[13:8];
+			fifo2 <= tx_data_i[23:14];
+			fifo3 <= tx_data_i[31:24];		
+			bit_counter <= 0;
+		end
+		else if(transfer_en_y)
+			begin
+				if(samp_ce_y)
+				begin
+					if(!msb_first)
+					begin
+						fifo0 <= {fifo1[0],fifo0[7:1]};
+						fifo1 <= {fifo2[0],fifo1[7:1]};
+						fifo2 <= {fifo3[0],fifo2[7:1]};
+						fifo3 <= {rx_bit_reg_y,fifo3[7:1]};
+					end
+					else
+					begin
+						fifo0 <= {fifo0[6:0],rx_bit_reg_y};
+						fifo1 <= {fifo0[6:0],fifo0[7]};
+						fifo2 <= {fifo0[6:0],fifo1[7]};
+						fifo3 <= {fifo0[6:0],fifo2[7]};
+					end
+					bit_counter <= bit_counter + 1;
+				end
+			end
+	end
+	
+	
+	
+//    /*=============================================================================================
+//    -- mosi generation
+//    =============================================================================================*/		
+
+	reg mosi_y;
+	reg [31:0] rx_data_y;
+	always@(posedge clk_i)
+	begin
+		if(transfer_en_y)
+		begin
+			if(samp_ce_y)
+				begin
+					if(!msb_first)
+						mosi_y <= fifo0[0];
+					else
+					begin
+						case(num_of_transferred_bits_y)
+							 6'd8		:	mosi_y <= fifo0[7];
+							 6'd16	:	mosi_y <= fifo1[7];
+							 6'd32	:	mosi_y <= fifo3[7];
+						endcase
+					end
+				end
+		end
+	end
+	
+	always@(*)
+	begin
+		if(!msb_first)
+		begin
+			case(num_of_transferred_bits_y)
+				 6'd8		: 	rx_data_y <= {24'd0,fifo3};
+				 6'd16	:	rx_data_y <= {16'd0,fifo3,fifo2};
+				 6'd32	:  rx_data_y <= {fifo3,fifo2,fifo1,fifo0};
+			endcase
+		end
+		else
+		begin
+			rx_data_y <= {fifo3,fifo2,fifo1,fifo0};
+		end
+	end
+	
+	
+//    /*=============================================================================================
+//    -- SCK out logic: pipeline phase compensation for the SCK line
+//    =============================================================================================*/	
+//
+
+	wire [3:0] ss_n = reg_trans_ctrl_i[3:0];
+
+	assign tx_pull_o = tx_pull_y;
+	assign trans_done_o = transfer_done_y;
+	assign rx_data_o = rx_data_y;
+	assign rx_push_o = rx_push_y;		
+	assign spi_ssel_o = (sclk_en_reg_y) ? ~(ss_n) : 4'hF; //bitwise
+	assign spi_sck_o = (!cpol_y) ? sclk_y : ~sclk_y;
+	assign spi_mosi_o = (sclk_en_reg_y) ? mosi_y : 1'bz;
 
 
 endmodule
